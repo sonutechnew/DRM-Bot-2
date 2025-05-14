91052ad6c28da239e6aa6a33a3524830e1a69e6d
sonutechnew <vaishnavit307@gmail.com>
2025-05-14 09:15:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from tglogging import TelegramLogHandler
 # Config 
 class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ.get("API_ID",  "27498866"))
+    API_ID = int(os.environ.get("API_ID",  ""))
     API_HASH = os.environ.get("API_HASH", "")
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     SESSIONS = "./SESSIONS"
